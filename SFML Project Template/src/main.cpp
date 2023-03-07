9d60137f58b8f81c00a49b3a8fa9a3c637704dbb
Buggyes <89547163+Buggyes@users.noreply.github.com>
2023-03-07 09:40:57 -0300

    It works (but better)

--- SFML Project Template/src/main.cpp
+++ SFML Project Template/src/main.cpp
@@ -1,6 +1,22 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-	sf::Window window(sf::VideoMode(600, 600), "sfml works");
+	sf::Texture texture;
+	if (!texture.loadFromFile("src/Sprites/Hard.png")) {
+		return EXIT_FAILURE;
+	}
+	sf::Sprite sprite(texture);
+	sf::RenderWindow window(sf::VideoMode(700, 400), "This image goes hard feel free to screenshot");
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+			window.clear();
+			window.draw(sprite);
+			window.display();
+		}
+	}
 	return 0;
 }
